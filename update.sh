f2bebdbac6707b5c208e378a56d4fcabbb3d2be3
Duncan494 <alexwright1324@gmail.com>
2022-01-26 19:01:15 +0000

    File Structure and update script
    File Structure with www and update script to be used on server.

---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /srv/http/sim.alexjameswright.net
+sudo -u http git pull
---
+++ www/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simulations</title>
+</head>
+<body>
+    <footer>Alex Wright and John Oakley</footer>
+</body>
+</html>

67e77fb7f9df8706628d7a1b77f44e3fd4196f0a
Duncan494 <alexwright1324@gmail.com>
2022-01-29 23:15:05 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,4 +2,5 @@
 
 cd /srv/http/sim.alexjameswright.net
 sudo -u http git fetch
-sudo -u http git reset --hard origin/deploy
+sudo -u http git reset --hard origin/master
+sudo -u http gulp -f gulp/gulpfile.js --cwd .

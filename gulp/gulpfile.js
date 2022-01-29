02c501eae0d1f871f5757d1b0a9eb1c6b04b0e32
Duncan494 <alexwright1324@gmail.com>
2022-01-29 23:10:20 +0000

    Gulp Fix

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,34 +1,118 @@
-var gulp = require('gulp'),
-    watch = require('gulp-watch'),
+const { src, dest, parallel, series, watch } = require('gulp'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    concat = require('gulp-concat'),
+    imagemin = require('gulp-imagemin'),
+    changed = require('gulp-changed'),
+    clean = require('gulp-clean'),
     rename = require('gulp-rename'),
     pug = require('gulp-pug'),
-    pugPHPFilter = require('pug-php-filter');
-    
-gulp.task('pug', function() {
-    return gulp.src('../www-src/**/*.pug')
-    .pipe( pug({
-        basedir: "../www-src/",
-        pretty: "\t",
-        filters: {
-        php: pugPHPFilter
-    }
-}) )
+    pugPHPFilter = require('pug-php-filter'),
+    phpConnect = require('gulp-connect-php'),
+    browsersync = require('browser-sync');
+
+const basedir = 'www-src/';
+const destination = 'site/';
+
+function clear() {
+    return src(destination, {
+            read: false,
+            allowEmpty: true
+        })
+        .pipe(clean());
+}
+
+// JS function 
+
+function js() {
+    const source = basedir + '**/*.js';
+
+    return src(source)
+        .pipe(changed(source))
+        .pipe(concat('bundle.js'))
+        .pipe(uglify())
+//        .pipe(rename({
+//            extname: '.min.js'
+//        }))
+        .pipe(dest(destination))
+        .pipe(browsersync.stream());
+}
+
+// CSS function 
+
+function css() {
+    const source = basedir + '**/*.css';
+
+    return src(source)
+        .pipe(changed(source))
+        //.pipe(sass())
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            cascade: false
+        }))
+//        .pipe(rename({
+//            extname: '.min.css'
+//        }))
+        .pipe(cssnano())
+        .pipe(dest(destination))
+        .pipe(browsersync.stream());
+}
+
+// Optimize images
+
+function img() {
+    return src([basedir + '**/*.png', basedir + '**/*.jpg'])
+        .pipe(imagemin())
+        .pipe(dest(destination))
+        .pipe(browsersync.stream());
+}
+
+// Pug PHP
+
+function pugphp() {
+    const source = basedir + '**/*.pug';
+    return src(source)
+        .pipe( pug({
+            basedir: basedir,
+            pretty: "\t",
+            filters: {
+            php: pugPHPFilter
+            }
+        }))
     .pipe(rename(function (path) {
         path.extname = ".php"
     }))
-    .pipe(gulp.dest('../site'));
+    .pipe(dest(destination))
+    .pipe(browsersync.stream());
+}
 
-});
-gulp.task('stream', function () {
-    // Endless stream mode
-    return watch('css/**/*.css', { ignoreInitial: false })
-        .pipe(gulp.dest('build'));
-});
- 
-gulp.task('callback', function () {
-    // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
-    return watch('css/**/*.css', function () {
-        gulp.src('css/**/*.css')
-            .pipe(gulp.dest('build'));
+function connectsync() {
+    phpConnect.server({
+        // a standalone PHP server that browsersync connects to via proxy
+        port: 3001,
+        keepalive: true,
+        base: destination
+    }, function (){
+        browsersync({
+            proxy: '127.0.0.1:3001'
+        });
     });
-});
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+    browsersync.reload();
+    done();
+}
+
+function watchFiles() {
+    watch(basedir + '**/*.pug', pugphp);
+    watch(basedir + '**/*.css', css);
+    watch(basedir + '**/*.js', js);
+    watch([basedir + '**/*.png', basedir + '**/*.jpg'], img);
+    watch(basedir + '**/*', browserSyncReload);
+}
+exports.watch = parallel(watchFiles, connectsync);
+exports.default = series(clear, parallel(pugphp, js, css, img));
--- site/index.php
+++ site/index.php
@@ -5,19 +5,21 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="/stylesheets/style.css">
-		<title>Sims - </title>
+		<title>Sims - Simulations</title>
 		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
 		<div class="main">
 			<nav>
 				<input id="switch" type="checkbox">
-				<label for="switch"></label><a href="/">Simulations</a>
+				<label for="switch"></label><a class="active" href="/">Simulations</a>
 				<ul>
 					<li><a href="/about/">About</a></li>
 				</ul>
-			</nav><a href="/half-life/" style="background-image: url('/images/radiation-counters.jpg')">
-				<div id="text">Half-Life Simulation</div></a>
+			</nav>
+			<ul class="links">
+				<li><a href="/half-life/" style="background-image: url('/images/radiation-counters.jpg')">Half-Life Simulation</a></li>
+			</ul>
 		</div>
 		<footer>Alex Wright and Jonathan Oakley</footer>
 	</body>
